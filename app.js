16694f089c2f4998e01bc65a04e7cff50f6fcca6
zumer305 <zumerniaz305@gmail.com>
2025-09-15 21:44:08 +0500

    update

--- app.js
+++ app.js
@@ -21,8 +21,40 @@ let post={
     content:"This is my first post.",
     likes:23,
     reposts:12,
-    tags:"#zum #er",
+    tags:["#zum","#er"],
 
 
 }
 console.log(post);
+console.log(post.content);
+console.log(post["content"]);
+console.log(post.tags[0]);
+
+
+let post2={
+    1:"@zumer",
+    2:"This is my first post.",
+    null:23,
+    undefined:12,
+
+}
+// console.log(post2.1); error
+console.log(post2["1"]);
+
+//  change
+post.likes=26;
+console.log(post);
+
+// add
+// console.log(post.coments);
+post.coments="yes";
+console.log(post);
+
+// delete
+delete post.coments;
+console.log(post);
+
+
+
+
+
