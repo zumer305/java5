f1221689dd5b92a518895c55ad30c7a339646560
zumer305 <zumerniaz305@gmail.com>
2025-09-15 21:04:58 +0500

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+// object literals (name,age,color) agy peechy ho skta
+let student=
+{
+    name:"zumer",
+    age:24,
+    color:"brown",
+}
+console.log(student);
+
+let student2=
+{
+    name:"zumer",
+    age:24,
+    color:["brown","pink"],
+}
+console.log(student2);
